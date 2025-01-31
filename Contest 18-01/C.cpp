dd8098c388e94209d8712c21d9230ee838313917
ian <ian.ribeiro4@gmail.com>
2025-01-30 23:13:44 -0300

    Primeiro contest 2025 renomeado

---
+++ Contest 18-01/C.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void ehQuad(int soma, int somaQuad){
+    if (soma != somaQuad){
+        printf("-1\n");
+        exit(0);
+    }
+}
+
+int main(){
+    int n; cin >> n;
+
+    int matriz[n][n], soma, somaQuad = 0;
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> matriz[i][j];
+
+    // Soma do Quadrado Magico
+    for (int i = 0; i < n; i++)
+        somaQuad += matriz[0][i]; // linha 1
+
+    // Soma linhas
+    for (int i = 0; i < n; i++){
+        soma = 0;
+
+        for (int j = 0; j < n; j++)
+            soma += matriz[i][j];
+        
+        ehQuad(soma, somaQuad);
+    }
+    
+    // Soma colunas
+    for (int i = 0; i < n; i++){
+        soma = 0;
+
+        for (int j = 0; j < n; j++)
+            soma += matriz[j][i];
+        
+        ehQuad(soma, somaQuad);
+    }
+
+    // Soma coluna principal
+    soma = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            if (i == j) soma += matriz[i][j];
+        
+    ehQuad(soma, somaQuad);
+        
+    // Soma coluna secundaria
+    soma = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            if (i + j == n-1) soma += matriz[i][j];
+        
+    ehQuad(soma, somaQuad);
+
+    printf("%i\n", somaQuad);
+
+    return 0;
+}
---
+++ Contest 18-01/F.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main(){
+    int a, m;
+    cin >> a >> m;
+
+    printf("%i\n", 2*m - a);
+
+    return 0;
+}
