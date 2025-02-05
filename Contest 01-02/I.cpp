ca68e90f91c91d166e8a39b913a5fb71f2bf16e9
ian <ian.ribeiro4@gmail.com>
2025-02-05 06:44:13 -0300

    Solucionado questão I

--- Contest 01-02/I.cpp
+++ Contest 01-02/I.cpp
@@ -4,38 +4,42 @@ using namespace std;
 typedef unsigned long long ull;
 
 char alternar(char c){
-    if (c == 'X')
-        return 'O';
-    else
-        return 'X';
+    return c == 'X' ? 'O' : 'X';
+    // if (c == 'X')
+    //     return 'O';
+    // else
+    //     return 'X';
 }
 
 int main(){
+
     int tc;
     cin >> tc;
+    
     ull c;
     string p;
-    int aux;
 
     while (tc--){
         cin >> p >> c;
         string ans;
 
-        if (c % 2 == 1)
-            ans += alternar(p[0]);
-        else
-            ans += p[0];
+        ans += c % 2 == 1 ? alternar(p[0]) : p[0];
+        // if (c % 2 == 1)
+        //     ans += alternar(p[0]);
+        // else
+        //     ans += p[0];
 
         for (int i = 1; i < p.size(); i++){
-            c /= 2;
             if (ans[i-1] == 'X')
-                ans += alternar(p[i]);
+                c++; // se o valor anterior trocou para X, vai ocorrer uma troca a mais
 
-            else if (c % 2 == 1 && c > 0)
-                ans += alternar(p[i]);
-            
-            else
-                ans += p[i];
+            c /= 2; // c = trocas
+
+            ans += c % 2 == 1 ? alternar(p[i]) : p[i];
+            // if (c % 2 == 1)
+            //     ans += alternar(p[i]);
+            // else
+            //     ans += p[i];
         }
         printf("%s\n", ans.c_str());
     }
