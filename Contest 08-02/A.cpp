6ced98c8c4f057a934104275c83def2c1779e1a6
ian <ian.ribeiro4@gmail.com>
2025-02-11 22:32:01 -0300

    Feynman

--- Contest 08-02/A.cpp
+++ Contest 08-02/A.cpp
@@ -6,14 +6,16 @@ typedef unsigned long long ull;
 
 int main(){
     int n;
-    while(1){
+
+    // Complexidade O (k * n), k = num de entradas
+    while(1){ 
         cin >> n;
 
         if (n == 0) break;
 
         ull ans = 1;
 
-        for (int i = 1; i < n; i++)
+        for (int i = 1; i < n; i++) // O(n)
             ans += (n-i+1) * (n-i+1);
         
         printf("%llu\n", ans);
