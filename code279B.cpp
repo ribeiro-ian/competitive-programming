c3cbe53e1643f5f96aed5bfc57d7cc17a3068c35
ian <ian.ribeiro4@gmail.com>
2025-03-31 13:14:27 -0300

    update

--- code279B.cpp
+++ code279B.cpp
@@ -17,18 +17,15 @@ int main() {
     for (int i = 0; i < n; i++) 
         scanf("%lli", &a[i]);
 
-    int ans = 0;
-    for (int i = 0; i < n; i++) {
-        ll sum = 0;
-        
-        for (int j = i; j < n; j++) {
-            sum += a[j];
-            
-            if (sum <= t) 
-                ans = max(ans, j-i+1);
-            else
-                break;
+    int ans = 0; ll sum = 0;
+    for (int i = 0, r = 0; i < n; i++) {
+        while (r < n && sum + a[r] <= t) {
+            sum += a[r];
+            r++;
         }
+
+        ans = max(ans, r - i);
+        sum -= a[i]; // remove o primeiro em vez de recomeçar a soma
     }
  
     printf("%i\n", ans);
