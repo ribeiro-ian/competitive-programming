4353c875d2f91ad14f4bf4b657c0ec558d37120e
ian <ian.ribeiro4@gmail.com>
2025-04-03 23:16:20 -0300

    Codeforces 1013 div3 problem D

---
+++ code2091D.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+ll tc, n, m, k;
+ 
+ll f(ll x) {
+    return n * (x * (m/(x+1)) + m % (x+1));
+}
+
+ll bs() {
+    ll l = 1, r = m, mid, ans = -1;
+
+    while (l <= r) {
+        mid = l+(r-l)/2;
+
+        fprintf(stderr, "f(%lli) = %lli\n", mid, f(mid));
+
+        if (f(mid) >= k) {
+            ans = mid;
+            r = mid - 1;
+        }
+        else {
+            l = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    // ios_base::sync_with_stdio(0); cin.tie(0);
+    cin >> tc;
+
+    while (tc--) {
+        cin >> n >> m >> k;
+
+        cout << bs() << '\n';
+        cerr << "----------------\n";
+    }
+    
+
+    return 0;
+}
