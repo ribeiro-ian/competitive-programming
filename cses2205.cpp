3308ccf5475fb61988097cb0a395cda6c5e4d083
Ian <ian.ribeiro4@gmail.com>
2025-06-05 18:18:26 -0300

    update

--- cses2205.cpp
+++ cses2205.cpp
@@ -9,27 +9,11 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
-void solve(int i, int v) {
-    if (i == 0) {
-        cout << '\n';
-        return;
-    }
-        
-
-    cout << v;
-    solve(i-1, 0);
-    cout << v;
-    solve(i-1, 1);
-} 
-
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
 
     int n; cin >> n;
 
-    // cout << "0\n1\n";
-
-    solve(n, 1);
 
     return 0;
 }
--- mineira2025E.cpp
+++ mineira2025E.cpp
@@ -8,29 +8,44 @@ using namespace std;
 
 typedef long long ll;
 typedef unsigned long long ull;
-typedef pair<ll,ll> pii;
+
+vector <vector <ll>> adj;
+vector <bool> visited;
+ll cnt;
+
+void dfs(ll u) {
+    visited[u] = true;
+    cnt++;
+    
+    for (auto &v : adj[u]) {
+        if (!visited[v])
+            dfs(v);
+    }
+}
 
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
 
     ll n, a, b; cin >> n;
-    map <pii, ll> mapa;
+    adj.resize(n+1);
+    visited.assign(n+1, false);
 
     for (ll i = 0; i < n; ++i) {
         cin >> a >> b;
 
-        if (a != b) mapa[{min(a,b), max(a,b)}] += 1;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
     }
 
-    ll resto = 0, ans = 0;
-    for (auto &m : mapa) {
-        if (m.second % 2 == 0)
-            ans += 1;
-        else
-            resto += 1;        
+    ll ans = 0;
+    for (int i = 1; i <= n; ++i) {
+        if (!visited[i]) {
+            cnt = 0;
+            dfs(i);
+            ans += cnt - 1;
+        }
     }
-    
-    if (resto) ans += resto - 1;
+
     cout << ans << '\n';
 
     return 0;
