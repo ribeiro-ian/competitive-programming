28d26b1224a2820877e7600d2be56388e69bd398
ian@cefet <ian.ribeiro4@gmail.com>
2026-04-17 16:57:37 -0300

    Balé feito

---
+++ neps/neps198.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n = 3;
+    int diagP = 0, diagS = 0, mat[n][n], lin[n] = {0}, col[n] = {0};
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> mat[i][j];
+
+            lin[i] += mat[i][j];
+            col[j] += mat[i][j];
+
+            if (i==j)
+                diagP += mat[i][j];
+            if (i+j==n-1)
+                diagS += mat[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        if (lin[i] != diagP || col[i] != diagP) {
+            cout << "NAO\n";
+            return 0;
+        }
+    }
+    if (diagS != diagP)
+        cout << "NAO\n";
+    else
+        cout << "SIM\n";
+
+    return 0;
+}
--- neps/neps536.cpp
+++ neps/neps536.cpp
@@ -1,29 +1,63 @@
+/*
+    OBI 2011 - Fase 2 - Balé
+    https://neps.academy/br/exercise/536
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
-typedef unsigned long long ull;
+ll ans = 0;
+ll merge(vector<ll>& v, ll l, ll mid, ll r) {
+    ll n = mid-l+1, m = r-mid;
+    ll left[n], right[m], cnt = 0;
 
-int value;
-bool func(int a){return a < value;}
+    for (ll i = 0; i < n; ++i)
+        left[i] = v[l+i];
+    for (ll i = 0; i < m; ++i)
+        right[i] = v[mid+1+i];
 
-// a mais habilidosa e a que frequenta a academia há mais tempo
-// menor i = mais tempo na academia
-// hab[i] = nivel de habilidade
+    ll i = 0, j = 0, k = l;
+    while (i < n && j < m) {
+        if (left[i] < right[j]) {
+            v[k++] = left[i++];
+        }
+        else {
+            cnt += n-i;
+            v[k++] = right[j++];
+        }
+    }
+    while (i < n)
+        v[k++] = left[i++];
+    while (j < m)
+        v[k++] = right[j++];
+    
+    return cnt;
+}
+
+ll mergesort(vector<ll>& v, ll l, ll r) {
+    if (l < r) {
+        ll m = l + (r-l)/2;
+
+        ans += mergesort(v,l,m);
+        ans += mergesort(v,m+1,r);
+        return merge(v,l,m,r);
+    }
+    else
+        return 0;
+}
 
 int main(){
-    ull n, cnt = 0;
-    cin >> n;
+    ios_base::sync_with_stdio(0); cin.tie(0);
 
-    vector <int> hab (n);
-    for (int i = 0; i < n; i++)
-       cin >> hab[i];
+    ll n; cin >> n;
+    vector <ll> v(n);
 
-    for (int i = 0; i < n-1; i++){
-        value = hab[i];
-        cnt += count_if(hab.begin()+i+1, hab.end(), func);
-    }
+    for (auto& i : v)
+       cin >> i;
 
-    printf("%li\n", cnt);
+    ans += mergesort(v,0,n-1);
+    cout << ans << '\n';
 
     return 0;
 }
