cc66b76a81746979e579e86f88a05b4044127a4f
ian <ian.ribeiro4@gmail.com>
2026-05-15 01:31:39 -0300

    update

--- uva/uva10440.cpp
+++ uva/uva10440.cpp
@@ -7,28 +7,26 @@
 using namespace std;
 using ll = long long;
 
-/*
-1
-2 10 5
-10
-40
-45
-50
-51
-
-10 -> volta 30
-40 45 -> volta 65
-50 51 -> volta 75
-*/
-void solve(){
+void solve() {
     int n, t, m, x;
     cin >> n >> t >> m;
 
     int a[n], time = 0, cnt = 0, trips = 0;
     for(int i = 0; i < m; ++i) 
         cin >> a[i];
-
-    cout << time << ' ' << trips << '\n';
+    
+    int i = 0, time = 0, trips = 0, cnt;
+    while (i < m) {
+        cnt = 0;
+        while (i < m && cnt < n) {
+            cnt++;
+            i++;
+        }
+        time = a[i-1]+2*t;
+        trips++;
+    }
+    
+    cout << time-t << ' ' << trips << '\n';
 }
 
 int main(){
